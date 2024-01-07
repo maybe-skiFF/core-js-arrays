e98cec66062152eb11baa657cafd942c3f156647
maybe-skiFF <den.skiff190@gmail.com>
2024-01-07 17:24:00 +0300

    feat: add doubleArray solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -221,8 +221,13 @@ function getTail(arr, n) {
  *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
  *    doubleArray([]) => []
  */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  // throw new Error('Not implemented');
+  if (arr.length === 0) {
+    return [];
+  }
+  const newArr = arr;
+  return arr.concat(newArr);
 }
 
 /**
