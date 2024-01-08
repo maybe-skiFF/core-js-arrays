3b7f7a1c65f6b204f11ec601dcb95a8afd8597d7
maybe-skiFF <den.skiff190@gmail.com>
2024-01-08 21:06:51 +0300

    feat: add generateOdds solution

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -368,8 +368,9 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  // throw new Error('Not implemented');
+  return Array.from(Array(len), (el, index) => index * 2 + 1);
 }
 
 /**
